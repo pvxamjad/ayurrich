199f845994d398c475cf592d5495e42fd4a2db7f
Amjad PV <amjadpvamd10@gmail.com>
2024-12-17 15:52:20 +0530

    first commit

---
+++ ar_store/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import Category,ProductRegistration,Contact
+
+
+
+admin.site.register(Category)
+admin.site.register(Contact)
+
+
+
+
+
+@admin.register(ProductRegistration)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'date_added',
+        'quantity'
+        )  # Add 'created_at' here to show in the list view.
+    readonly_fields = ('date_added',)  # Mark 'created_at' as readonly in the admin form.
